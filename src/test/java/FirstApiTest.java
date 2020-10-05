7fceb4ed7edc45a3ca207cdcba2c2611553c57e5
troiansasha <troianwork@gmail.com>
2020-10-05 17:17:00 +0300

    API All Requests 1

--- src/test/java/FirstApiTest.java
+++ src/test/java/FirstApiTest.java
@@ -1,11 +1,17 @@
+import io.restassured.RestAssured;
 import io.restassured.response.Response;
+import io.restassured.specification.RequestSpecification;
+import org.json.JSONObject;
+import org.testng.Assert;
 import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 
 import java.sql.Timestamp;
 import java.util.Date;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
+import java.util.stream.Stream;
 
 import static io.restassured.RestAssured.given;
 import static org.hamcrest.Matchers.equalTo;
@@ -29,13 +35,13 @@ public class FirstApiTest {
                         .response();
         response1.getBody().print();
     }
+
     @Test()
     public void testPOST() {
 
         Map<String, String> data = new HashMap<>();
         data.put("orderId", "2");
         given()
-//                .contentType("application/json")
                 .body(data)
                 .when()
                 .post("http://httpbin.org/post").then()
@@ -43,7 +49,8 @@ public class FirstApiTest {
 
                 .body("json.orderId", equalTo("2"));
     }
-//Test weather
+
+    //Test weather
     @Test()
     public void testWeatherLondon_GET() {
         Response response1 =
@@ -62,6 +69,7 @@ public class FirstApiTest {
                         .response();
         response1.getBody().print();
     }
+
     @Test()
     public void testWeatherLondon2_GET() {
         Response response1 =
@@ -73,8 +81,8 @@ public class FirstApiTest {
                         .log().all()
                         .statusCode(200)
 //                        .body("sys.country", equalTo("GB"))
-                        .body("weather.id[0]", equalTo(804))
-                        .body("weather.icon[0]", equalTo("02n"))
+                        .body("weather.id[0]", equalTo(802))
+                        .body("weather.icon[0]", equalTo("03d"))
                         .body("id", equalTo(2643743))
                         .extract()
                         .response();
@@ -83,7 +91,7 @@ public class FirstApiTest {
 
     //Privat Bank
     @Test()
-    public void testPrivat_id1_GET(){
+    public void testPrivat_id1_GET() {
         Response response1 =
                 given()
                         .log().all()
@@ -93,13 +101,14 @@ public class FirstApiTest {
                         .log().all()
                         .statusCode(200)
                         .body("ccy[0]", equalTo("BTC"))
-                         .body("sale[0]", greaterThanOrEqualTo("11000"))
+                        .body("sale[0]", greaterThanOrEqualTo("10000"))
                         .extract()
                         .response();
         response1.getBody().print();
     }
+
     @Test()
-    public void testPrivat_id5_GET(){
+    public void testPrivat_id5_GET() {
         Response response1 =
                 given()
                         .log().all()
@@ -109,7 +118,7 @@ public class FirstApiTest {
                         .log().all()
                         .statusCode(200)
                         .body("ccy[1]", equalTo("EUR"))
-                        .body("sale[2]", equalTo("0.38000"))
+                        .body("sale[2]", equalTo("0.37700"))
                         .extract()
                         .response();
         response1.getBody().print();
@@ -118,7 +127,7 @@ public class FirstApiTest {
 
     //List Of Users
     @Test()
-    public void listOfUsers_GET(){
+    public void listOfUsers_GET() {
         Response response1 =
                 given()
                         .log().all()
@@ -140,9 +149,10 @@ public class FirstApiTest {
                         .response();
         response1.getBody().print();
     }
+
     //User not found +
     @Test()
-    public void userError_GET(){
+    public void userError_GET() {
         Response response1 =
                 given()
                         .log().all()
@@ -157,25 +167,135 @@ public class FirstApiTest {
     }
 //Create User PostRequest
 
-//@Test()
-//public void createUser_POST() {
-//
-//    Map<String, String> data = new HashMap<>();
-//    data.put("name", "morpheus");
-//    data.put("job", "leader");
-//    given()
-////                .contentType("application/json")
-//            .body(data)
-//            .when()
-//            .post("https://reqres.in/api/users").then()
-//            .statusCode(201)
-//            .body("name", equalTo("morpheus"))
-//            .body("job", equalTo("leader"))
-//            .body("id", equalTo("885"));
-//}
+    @Test
+    public void createUser_POST () {
 
+
+        JSONObject requestBody = new JSONObject();
+        requestBody.put("name", "morpheus");
+        requestBody.put("job", "leader");
+
+        RequestSpecification request = RestAssured.given();
+        request.header("Content-Type", "application/json");
+        request.body(requestBody.toString());
+        Response response = request.post("https://reqres.in/api/users");
+
+        int statusCode = response.getStatusCode();
+        Assert.assertEquals(statusCode, 201);
+
+        String successName = response.jsonPath().get("name");
+        Assert.assertEquals(successName, "morpheus");
+        String successJob = response.jsonPath().get("job");
+        Assert.assertEquals(successJob, "leader");
+        System.out.println(response.getBody().asString());
 
     }
+
+    //Update user Put Request
+    @Test
+    public void updateUser_PUT () {
+
+
+        JSONObject requestBody = new JSONObject();
+        requestBody.put("name", "morpheus");
+        requestBody.put("job", "zion resident");
+
+        RequestSpecification request = RestAssured.given();
+        request.header("Content-Type", "application/json");
+        request.body(requestBody.toString());
+        Response response = request.put("https://reqres.in/api/users/2");
+
+        int statusCode = response.getStatusCode();
+        Assert.assertEquals(statusCode, 200);
+
+        String successName = response.jsonPath().get("name");
+        Assert.assertEquals(successName, "morpheus");
+        String successJob = response.jsonPath().get("job");
+        Assert.assertEquals(successJob, "zion resident");
+        System.out.println(response.getBody().asString());
+
+    }
+    //Delete user Put Request
+    @Test
+    public void deleteUser_PUT () {
+
+        RequestSpecification request = RestAssured.given();
+        request.header("Content-Type", "application/json");
+        Response response = request.delete("https://reqres.in/api/users/2");
+
+        int statusCode = response.getStatusCode();
+        Assert.assertEquals(statusCode, 204);
+
+    }
+    //Register user Post Request
+        @Test
+        public void registerUser_POST () {
+
+
+            JSONObject requestBody = new JSONObject();
+            requestBody.put("email", "eve.holt@reqres.in");
+            requestBody.put("password", "pistol");
+
+            RequestSpecification request = RestAssured.given();
+            request.header("Content-Type", "application/json");
+            request.body(requestBody.toString());
+            Response response = request.post("https://reqres.in/api/register");
+
+            int statusCode = response.getStatusCode();
+            Assert.assertEquals(statusCode, 200);
+
+            int successId = response.jsonPath().get("id");
+            Assert.assertEquals(successId, 4);
+            String successToken = response.jsonPath().get("token");
+            Assert.assertEquals(successToken, "QpwL5tke4Pnpja7X4");
+            System.out.println(response.getBody().asString());
+
+        }
+    @Test
+    public void registerUserUnsuccesfull_POST () {
+
+
+        JSONObject requestBody = new JSONObject();
+        requestBody.put("email", "eve.holt@reqres.in");
+        //Empty Password
+
+        RequestSpecification request = RestAssured.given();
+        request.header("Content-Type", "application/json");
+        request.body(requestBody.toString());
+        Response response = request.post("https://reqres.in/api/register");
+
+        int statusCode = response.getStatusCode();
+        Assert.assertEquals(statusCode, 400);
+
+        String successToken = response.jsonPath().get("error");
+        Assert.assertEquals(successToken, "Missing password");
+        System.out.println(response.getBody().asString());
+
+    }
+    @Test
+    public void LoginSuccesfull_POST () {
+
+
+        JSONObject requestBody = new JSONObject();
+        requestBody.put("email", "eve.holt@reqres.in");
+        requestBody.put("password", "cityslicka");
+
+        RequestSpecification request = RestAssured.given();
+        request.header("Content-Type", "application/json");
+        request.body(requestBody.toString());
+        Response response = request.post("https://reqres.in/api/login");
+
+        int statusCode = response.getStatusCode();
+        Assert.assertEquals(statusCode, 200);
+
+        String successToken = response.jsonPath().get("token");
+        Assert.assertEquals(successToken, "QpwL5tke4Pnpja7X4");
+        System.out.println(response.getBody().asString());
+
+    }
+
+    }
+
 
 
 
